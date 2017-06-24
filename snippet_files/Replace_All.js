165a49fae418e80c7cb8c5e5f34aa26b9970c924
dev-rj8 <dev.rjakkani8@gmail.com>
2017-06-23 19:59:55 -0700

    Replace function in JS

---
+++ snippet_files/Replace_All.js
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html><body>
+<script>
+
+var str = "A Hello, World! program is a computer program that outputs or displays Hello, World! to a user.";
+
+//replace first occurance World with JS
+str = str.replace("World", "JS");
+document.write(str+"<br>");
+
+//replace all Hello words with Hell
+str = str.replace(/Hello/g, "Hell");
+console.log(str);
+
+document.write(str);
+</script>
+
+</body></html> 
